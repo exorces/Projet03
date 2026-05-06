c37907c4aa8cb69ad79c5a63dd9d000c8d389174
Ken <superkenli01@gmail.com>
2026-05-06 15:16:04 -0400

    changer le footer

--- _partials/footer.php
+++ _partials/footer.php
@@ -1,7 +1,7 @@
   </main>
 
   <footer class="app-footer">
-    <span>Équipe XYZ · 420-W46 · Hiver 2026</span>
+    <span>Équipe Rayane et Ken · 420-W46 · Hiver 2026</span>
     <span>Cégep Gérald-Godin</span>
   </footer>
 
